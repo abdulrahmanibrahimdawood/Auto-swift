4cd907289b4e1ad0207f181de05a16a88cdcd79a
abdulrahmanibrahimdawood <kabdo2223@gmail.com>
2026-01-30 18:49:32 +0200

    change app icon & integrate supabase

---
+++ lib/features/admin/admin_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AdminView extends StatelessWidget {
+  const AdminView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
